45666600e6f3d4de473390c105cdf76636412b14
Eurobeater <antoniogallegopenalver@gmail.com>
2024-10-22 18:51:27 +0200

    Cambios en la jerarquía de los directorios

--- Programacion de Inteligencia Artificial/Tema 2/prueba.py
+++
@@ -1,9 +0,0 @@
-print("Hola mundo")
-
-numeroInt = 20
-numeroDouble = 20.5
-booleano = False
-
-print(numeroInt)
-print(numeroDouble)
-print(booleano)
---
+++ Programacion de Inteligencia Artificial/prueba.py
@@ -0,0 +1,9 @@
+print("Hola mundo")
+
+numeroInt = 20
+numeroDouble = 20.5
+booleano = False
+
+print(numeroInt)
+print(numeroDouble)
+print(booleano)
