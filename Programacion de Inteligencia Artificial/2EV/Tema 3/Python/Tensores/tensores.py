53e408692395ae75bf168712fd787c286caac489
Eurobeater <antoniogallegopenalver@gmail.com>
2025-01-24 21:10:43 +0100

    PIA Tema 3 añadido

---
+++ Programacion de Inteligencia Artificial/2EV/Tema 3/Python/Tensores/tensores.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+tensor_rango_0 = np.array(5)
+print(tensor_rango_0)
+print(type(tensor_rango_0))
+
+tensor_rango_1 = np.array([1, 2, 3])
+print(tensor_rango_1)
+print(type(tensor_rango_1))
+
+tensor_rango_2 = np.array([1, 2, 3], [4, 5, 6])
+print(tensor_rango_2)
+print(type(tensor_rango_2))
+
+tensor_rango_3 = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+print(tensor_rango_3)
+print(type(tensor_rango_3))
